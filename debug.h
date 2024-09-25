0d95eaf75274b62d8270064177d3f504edc4f20e
AaronWu-train <aaronwu0218@gmail.com>
2024-09-25 17:58:02 +0800

    Add description to debug.h

--- debug.h
+++ debug.h
@@ -1,3 +1,13 @@
+// File: debug.h
+// Author: Aaron Wu (B13901011)
+// Date: 2024-09-25
+// Description: include this file and add -DDEBUG flag to compile with debug output
+// Usage: debug("hello world", x, y, x + y);
+//        OI(vec.begin(), vec.end()); // output iterator
+
+#pragma once
+#include <iostream>
+#define AI(x) begin(x), end(x)
 #ifdef DEBUG 
 #define debug(args...) LKJ("\033[1;32m[ "+string(#args)+" ]\033[0m", args)
 template<class I> void LKJ(I&&x){ cerr << x << '\n'; }
