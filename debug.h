4c87469cf80d751e425371ed107fd792c3f0cf4c
Aaron Wu <99801904+AaronWu-train@users.noreply.github.com>
2024-09-25 17:52:22 +0800

    Update debug.h

--- debug.h
+++ debug.h
@@ -1,10 +1,3 @@
-#pragma GCC optimize("Ofast")
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define pb push_back
-#define endl '\n'
-#define AI(x) begin(x),end(x)
 #ifdef DEBUG 
 #define debug(args...) LKJ("\033[1;32m[ "+string(#args)+" ]\033[0m", args)
 template<class I> void LKJ(I&&x){ cerr << x << '\n'; }
@@ -14,4 +7,3 @@ template<class I> void OI(I a, I b){ while(a < b) cerr << *a << " \n"[next(a) ==
 #define debug(...) 0
 #define OI(...) 0
 #endif
-#define _ ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
